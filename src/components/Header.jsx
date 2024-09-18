dfd9549c3fd78fd240c140304fbdf849d99b6e6e
Muhammad Abdullah <sameeralvi156@gmail.com>
2024-09-18 16:26:24 +0500

    updated images path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,19 @@ const Header = () => {
       <header className="hero">
         <div className="hero-center">
           <div className="underline"></div>
-          <h1>I'm, abdullah</h1>
-          <p className="banner-info">
-            I am a web developer. I can put your imagination into technology. I
-            will be your creative web development partner.
-          </p>
-          <a
-            href="https://drive.google.com/file/d/1fbOzLjPW-WhM1rFshwdGmfsvoNPJMoIL/view?usp=drive_link"
-            target="_blank"
-          >
-            <button className="btn">Download CV</button>
-          </a>
+          <div className="banner-content">
+            <h1>I'm, abdullah</h1>
+            <p className="banner-info">
+              I am a web developer. I can put your imagination into technology.
+              I will be your creative web development partner.
+            </p>
+            <a
+              href="https://drive.google.com/file/d/1fbOzLjPW-WhM1rFshwdGmfsvoNPJMoIL/view?usp=drive_link"
+              target="_blank"
+            >
+              <button className="btn">Download CV</button>
+            </a>
+          </div>
         </div>
       </header>
     </section>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useGlobalContext } from "../components/context";
 import Loading from "../components/Loading";
+import Navbar from "../components/Navbar";
 
 const Projects = () => {
   const projectImages = {
-    "Github Users Search": "../public/Github Users Search.png",
-    "React Search Photos": "../public/React Search Photos.png",
-    "React Quiz App": "../public/React Quiz App.png",
+    "Github Users Search": "/Github Users Search.png",
+    "React Search Photos": "/React Search Photos.png",
+    "React Quiz App": "/React Quiz App.png",
   };
   const livePreview = {
     "Github Users Search": "https://searchusersfromgithub.netlify.app",
@@ -25,49 +26,51 @@ const Projects = () => {
     return <Loading />;
   }
   return (
-    <section className="section">
-      <div className="section-title">
-        <h2>my projects</h2>
-        <div className="underline"></div>
-      </div>
-      <div className="section-center">
-        <div className="project-card-container">
-          {filteredRepos.map((card) => {
-            const { id, name, html_url, language, description } = card;
-            const formattedName = name
-              .replace(/-/g, " ")
-              .replace(/\b\w/g, (char) => char.toUpperCase());
-
-            const imageSrc =
-              projectImages[formattedName] || "../public/default.jpg";
-            const liveSrc = livePreview[formattedName];
-            return (
-              <article key={id} className="project-card">
-                <div className="card-side card-front">
-                  <img src={imageSrc} className="project-img" />
-                  <div className="project-info">
-                    <h3 className="project-title">{formattedName}</h3>
-                    <p>
-                      {description ? description : "No description available"}
-                    </p>
-                  </div>
-                </div>
-                <div className="card-side card-back">
-                  <div className="links-container">
-                    <a href={html_url} target="_blank">
-                      Source Code
-                    </a>
-                    <a href={liveSrc} target="_blank">
-                      Live Preview
-                    </a>
-                  </div>
-                </div>
-              </article>
-            );
-          })}
+    <>
+      <Navbar />
+      <section className="section">
+        <div className="section-title">
+          <h2>my projects</h2>
+          <div className="underline"></div>
         </div>
-      </div>
-    </section>
+        <div className="section-center">
+          <div className="project-card-container">
+            {filteredRepos.map((card) => {
+              const { id, name, html_url, language, description } = card;
+              const formattedName = name
+                .replace(/-/g, " ")
+                .replace(/\b\w/g, (char) => char.toUpperCase());
+
+              const imageSrc = projectImages[formattedName] || "/default.jpg";
+              const liveSrc = livePreview[formattedName];
+              return (
+                <article key={id} className="project-card">
+                  <div className="card-side card-front">
+                    <img src={imageSrc} className="project-img" />
+                    <div className="project-info">
+                      <h3 className="project-title">{formattedName}</h3>
+                      <p>
+                        {description ? description : "No description available"}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="card-side card-back">
+                    <div className="links-container">
+                      <a href={html_url} target="_blank">
+                        Source Code
+                      </a>
+                      <a href={liveSrc} target="_blank">
+                        Live Preview
+                      </a>
+                    </div>
+                  </div>
+                </article>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
